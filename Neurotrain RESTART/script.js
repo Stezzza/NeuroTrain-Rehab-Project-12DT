834cd53d9c9bea0ad698848e106710fe23599d2f
lucaalmond <luca.almond.j@gmail.com>
2025-08-06 09:38:05 +0930

    more js, handling mouse movement

--- Neurotrain RESTART/script.js
+++ Neurotrain RESTART/script.js
@@ -165,3 +165,94 @@ function create_hold_target() {
     };
     game_screen.appendChild(target);
 }
+
+function create_drag_target() {
+    var obj_size = { w: 80, h: 40 };
+    var zone_size = parseInt(target_size_select.value);
+    var obj_pos = get_random_position(obj_size);
+    var zone_pos = get_random_position({w: zone_size, h: zone_size});
+
+    drag_object = document.createElement('div');
+    drag_object.className = 'drag-obj';
+    drag_object.style.width = obj_size.w + 'px';
+    drag_object.style.height = obj_size.h + 'px';
+    drag_object.style.left = obj_pos.x + 'px';
+    drag_object.style.top = obj_pos.y + 'px';
+    drag_object.textContent = 'drag me';
+    
+    drag_zone_object = document.createElement('div');
+    drag_zone_object.className = 'drag-zone';
+    drag_zone_object.style.width = zone_size + 'px';
+    drag_zone_object.style.height = zone_size + 'px';
+    drag_zone_object.style.left = zone_pos.x + 'px';
+    drag_zone_object.style.top = zone_pos.y + 'px';
+    
+    drag_object.onmousedown = function(event) {
+        event.stopPropagation();
+        is_dragging = true;
+        reaction_times.push(Date.now() - target_appear_time);
+        drag_start_pos = { x: event.clientX, y: event.clientY };
+        drag_mouse_path = [drag_start_pos];
+    };
+    game_screen.appendChild(drag_zone_object);
+    game_screen.appendChild(drag_object);
+}
+
+// input handling
+
+function handle_mouse_move(event) {
+    if (is_dragging && drag_object) {
+        var game_rect = game_screen.getBoundingClientRect();
+        drag_object.style.left = (event.clientX - game_rect.left - 40) + 'px';
+        drag_object.style.top = (event.clientY - game_rect.top - 20) + 'px';
+        drag_mouse_path.push({x: event.clientX, y: event.clientY});
+    }
+    if (is_holding) {
+        var target = game_screen.querySelector('.target.holding');
+        if (!target) return;
+        var rect = target.getBoundingClientRect();
+        var size = rect.width;
+        var mouse_x = event.clientX - rect.left;
+        var mouse_y = event.clientY - rect.top;
+        var dist = Math.sqrt(Math.pow(mouse_x - size / 2, 2) + Math.pow(mouse_y - size / 2, 2));
+        current_tremor_samples.push(dist);
+    }
+}
+
+function handle_mouse_up() {
+    if (is_dragging) {
+        is_dragging = false;
+        if (!drag_object || !drag_zone_object) return;
+        var obj_rect = drag_object.getBoundingClientRect();
+        var zone_rect = drag_zone_object.getBoundingClientRect();
+        var is_overlap = !(obj_rect.right < zone_rect.left || obj_rect.left > zone_rect.right || obj_rect.bottom < zone_rect.top || obj_rect.top > zone_rect.bottom);
+
+        if (is_overlap) {
+            record_hit();
+            var start = drag_start_pos;
+            var end = {x: zone_rect.left + zone_rect.width/2, y: zone_rect.top + zone_rect.height/2};
+            var straight_dist = Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2));
+            var actual_dist = 0;
+            for (var i = 1; i < drag_mouse_path.length; i++) {
+                actual_dist += Math.sqrt(Math.pow(drag_mouse_path[i].x - drag_mouse_path[i-1].x, 2) + Math.pow(drag_mouse_path[i].y - drag_mouse_path[i-1].y, 2));
+            }
+            if (actual_dist > 0) drag_efficiencies.push(straight_dist / actual_dist);
+
+        } else {
+            record_miss();
+        }
+        drag_object.remove();
+        drag_zone_object.remove();
+        drag_object = null;
+        spawn_target();
+    }
+
+    if (is_holding) {
+        is_holding = false;
+        clearTimeout(hold_timer);
+        record_miss();
+        var target = game_screen.querySelector('.target.hold');
+        if (target) target.remove();
+        spawn_target();
+    }
+}
